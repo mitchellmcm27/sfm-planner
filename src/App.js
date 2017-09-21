9008daf39e597f715448622fe39c71d774a94e9c
Mitchell McMillan <mitchellmcm27@gmail.com>
2017-09-20 21:49:57 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React, { Component } from "react";
 import Line from "react-lineto";
 import { colorInterpolation } from "color-interpolator";
+import DebounceInput from "react-debounce-input";
 
 import logo from "./logo.svg";
 import "./App.css";
+
+const debounceTimeout = 500;
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       resolution: "0.03", // m/px
-      overlap: "0.75",
+      overlapX: "0.75",
+      overlapY: "0.75",
       speed: "10", // m/s
-      dx: "3000", // px
-      dy: "4000", // px
-      fov: "78.8" // degrees
+      dx: "4000", // px
+      dy: "3000", // px
+      fov: "78.8", // degrees
+      bits: "14", // bits per pixel
+      surveyDx: "500", // m
+      surveyDy: "500", // m
+      extraPhotos: "0" // number of photos to add on the edges of the survey
     };
   }
 
@@ -27,9 +35,7 @@ class App extends Component {
     });
   };
 
-  renderFootprints(height, width) {
-    const nrows = 12;
-    const ncols = 6;
+  renderFootprints(height, width, nrows, ncols) {
     let rows = new Array(nrows);
     let cols = new Array(ncols);
 
@@ -48,8 +54,8 @@ class App extends Component {
               className={"A" + i + j}
               style={{
                 position: "absolute",
-                top: height * (1 - this.state.overlap) * i,
-                left: width * (1 - this.state.overlap) * j,
+                top: height * (1 - this.state.overlapY) * i,
+                left: width * (1 - this.state.overlapX) * j,
                 width: width,
                 height: height,
                 background: "blue",
@@ -61,8 +67,8 @@ class App extends Component {
                 className={"A" + i + j}
                 style={{
                   position: "absolute",
-                  top: height * (1 - this.state.overlap) * i - 2,
-                  left: width * (1 - this.state.overlap) * j - 2,
+                  top: height * (1 - this.state.overlapY) * i - 2,
+                  left: width * (1 - this.state.overlapX) * j - 2,
                   width: width,
                   height: height,
                   border: "2px solid black",
@@ -78,7 +84,7 @@ class App extends Component {
                     textAlign: "left"
                   }}
                 >
-                  {this.state.dx * this.state.resolution + " m"}
+                  {height + " m"}
                 </span>
                 <span
                   style={{
@@ -88,14 +94,38 @@ class App extends Component {
                     textAlign: "center"
                   }}
                 >
-                  {this.state.dy * this.state.resolution + " m"}
+                  {width + " m"}
                 </span>
               </span>
             )}
+            {last && this.renderSurveyArea(height, width)}
           </div>
         );
       });
     });
+  }
+
+  renderSurveyArea(height, width) {
+    return (
+      <div
+        style={{
+          position: "absolute",
+          left:
+            this.state.extraPhotos * 1 * (1 - this.state.overlapX) * width / 2 +
+            width / 2,
+          top:
+            this.state.extraPhotos *
+              1 *
+              (1 - this.state.overlapY) *
+              height /
+              2 +
+            height / 2,
+          width: this.state.surveyDx,
+          height: this.state.surveyDy,
+          border: "3px solid red"
+        }}
+      />
+    );
   }
 
   renderFootprint(height, width) {
@@ -117,19 +147,27 @@ class App extends Component {
       2 /
       Math.tan(this.state.fov * Math.PI / 180 / 2)).toFixed(2);
 
-    const height = this.state.dx * this.state.resolution;
-    const width = this.state.dy * this.state.resolution;
+    const height = this.state.dy * this.state.resolution;
+    const width = this.state.dx * this.state.resolution;
 
     const frameInterval = (height *
-      (1 - this.state.overlap) /
+      (1 - this.state.overlapY) /
       this.state.speed).toFixed(2);
+
+    const nrows =
+      Math.ceil(this.state.surveyDy / height / (1 - this.state.overlapY)) +
+      this.state.extraPhotos * 1;
+    const ncols =
+      Math.ceil(this.state.surveyDx / width / (1 - this.state.overlapX)) +
+      this.state.extraPhotos * 1;
 
     return (
       <div style={{ height: "100%", width: "100%", padding: 12 }}>
-        <form style={{ float: "left", width: "50%" }}>
+        <form style={{ float: "left" }}>
           <label>
             Desired resolution:
-            <input
+            <DebounceInput
+              debounceTimeout={debounceTimeout}
               type="text"
               name="resolution"
               value={this.state.resolution}
@@ -139,11 +177,60 @@ class App extends Component {
           </label>
           <br />
           <label>
-            Desired image overlap:
-            <input
+            Survey size (x):
+            <DebounceInput
+              debounceTimeout={debounceTimeout}
               type="text"
-              name="overlap"
-              value={this.state.overlap}
+              name="surveyDx"
+              value={this.state.surveyDx}
+              onChange={this.handleInputChange}
+            />
+            meters
+          </label>
+          <br />
+          <label>
+            Survey size (y):
+            <DebounceInput
+              debounceTimeout={debounceTimeout}
+              type="text"
+              name="surveyDy"
+              value={this.state.surveyDy}
+              onChange={this.handleInputChange}
+            />
+            meters
+          </label>
+          <br />
+          <label>
+            Desired image overlap (x):
+            <DebounceInput
+              debounceTimeout={debounceTimeout}
+              type="text"
+              name="overlapX"
+              value={this.state.overlapX}
+              onChange={this.handleInputChange}
+            />
+            percent
+          </label>
+          <br />
+          <label>
+            Desired image overlap (y):
+            <DebounceInput
+              debounceTimeout={debounceTimeout}
+              type="text"
+              name="overlapY"
+              value={this.state.overlapY}
+              onChange={this.handleInputChange}
+            />
+            percent
+          </label>
+          <br />
+          <label>
+            Extra layers of photos on edges:
+            <DebounceInput
+              debounceTimeout={debounceTimeout}
+              type="text"
+              name="extraPhotos"
+              value={this.state.extraPhotos}
               onChange={this.handleInputChange}
             />
             percent
@@ -151,7 +238,8 @@ class App extends Component {
           <br />
           <label>
             Drone speed:
-            <input
+            <DebounceInput
+              debounceTimeout={debounceTimeout}
               type="text"
               name="speed"
               value={this.state.speed}
@@ -162,7 +250,8 @@ class App extends Component {
           <br />
           <label>
             Image dimension (x):
-            <input
+            <DebounceInput
+              debounceTimeout={debounceTimeout}
               type="text"
               name="dx"
               value={this.state.dx}
@@ -173,7 +262,8 @@ class App extends Component {
           <br />
           <label>
             Image dimension (y):
-            <input
+            <DebounceInput
+              debounceTimeout={debounceTimeout}
               type="text"
               name="dy"
               value={this.state.dy}
@@ -184,13 +274,26 @@ class App extends Component {
           <br />
           <label>
             Camera field of view:
-            <input
+            <DebounceInput
+              debounceTimeout={debounceTimeout}
               type="text"
               name="fov"
               value={this.state.fov}
               onChange={this.handleInputChange}
             />
             degrees
+          </label>
+          <br />
+          <label>
+            Image bit depth:
+            <DebounceInput
+              debounceTimeout={debounceTimeout}
+              type="text"
+              name="bits"
+              value={this.state.bits}
+              onChange={this.handleInputChange}
+            />
+            bits per pixel
           </label>
           <h3>
             {isNaN(droneHeight) ? (
@@ -207,14 +310,38 @@ class App extends Component {
               `Photo interval: ${frameInterval} seconds`
             )}
           </h3>
+
+          <h3>
+            {isNaN(nrows * ncols) ? (
+              "Check input"
+            ) : (
+              `Number of photos: ${nrows * ncols} (${nrows}x${ncols})`
+            )}
+          </h3>
+
+          <h3>
+            {isNaN(
+              nrows * ncols * this.state.dx * this.state.dy * this.state.bits
+            ) ? (
+              "Check input"
+            ) : (
+              `Disk space: ${(nrows *
+                ncols *
+                this.state.dx *
+                this.state.dy *
+                this.state.bits /
+                8 /
+                1000000000).toFixed(2)} GB (${this.state.bits}-bit DNG)`
+            )}
+          </h3>
         </form>
-        <div style={{ float: "left", width: "50%" }}>
+        <div style={{ float: "left", paddingLeft: 24 }}>
           <h3>Single photo</h3>
           {this.renderFootprint(height, width)}
 
           <h3>Survey area</h3>
           <div style={{ position: "relative" }}>
-            {this.renderFootprints(height, width)}
+            {this.renderFootprints(height, width, nrows, ncols)}
           </div>
         </div>
       </div>
